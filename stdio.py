2512b3c9a4d462e0102102bc9e1dbb87703bfbcd
simon3000 <simon3000@163.com>
2019-09-08 06:26:17 +0200

    Create stdio.py

---
+++ stdio.py
@@ -0,0 +1,19 @@
+import sys
+from fans_kalman import fans_kalman
+
+sigma_w = 0.5
+sigma_v = 0.2223
+
+kalman_filter = None
+
+for line in sys.stdin:
+  try:
+    num = int(line)
+  except Exception:
+    if kalman_filter != None:
+      print(kalman_filter.predict_interface(None)[0])
+  else:
+    if kalman_filter == None:
+      kalman_filter = fans_kalman(num, sigma_w=sigma_w, sigma_v=sigma_v)
+    else:
+      kalman_filter.predict_interface(num)
