028ff65d7a729f5712a37b50c3d6785b279fd93d
simon3000 <simon3000@163.com>
2019-09-08 06:55:59 +0200

    print flush

--- stdio.py
+++ stdio.py
@@ -11,9 +11,9 @@ for line in sys.stdin:
     num = int(line)
   except Exception:
     if kalman_filter != None:
-      print(kalman_filter.predict_interface(None)[0])
+      print(kalman_filter.predict_interface(None)[0], flush=True)
   else:
     if kalman_filter == None:
       kalman_filter = fans_kalman(num, sigma_w=sigma_w, sigma_v=sigma_v)
     else:
-      print(kalman_filter.predict_interface(num)[0])
+      print(kalman_filter.predict_interface(num)[0], flush=True)
